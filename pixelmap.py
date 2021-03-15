bd0ce32a3a50f44d7d4795af87d01656e1661536
Alex D. Zharichenko <azharichenko@gmail.com>
2021-03-15 11:39:13 -0400

    Switched script to blue and it now works

--- pixelmap.py
+++ pixelmap.py
@@ -8,52 +8,55 @@ import imutils
 import serial
 import time
 
-RED = [1, 0, 0]
+BLUE = [0, 0, 255]
 BLACK = [0, 0, 0]
 
 
 def draw_calibration_pixel(led_number, retries=3):
     with serial.Serial("COM3", baudrate=2152000, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE) as ser:
         for _ in range(retries):
-            ser.write(bytearray(BLACK * led_number + RED + BLACK * (299 - led_number)))
-            time.sleep(0.3)
+            ser.write(bytearray(BLACK * led_number + BLUE + BLACK * (299 - led_number)))
+            time.sleep(.3)
 
 
 LED_NUM = 300
-output = Path.cwd() / "led_pos.csv"
+output = Path.cwd() / "led_pos.4.csv"
 
 
-def locate_led_coord(cap, frames=10):
+def locate_led_coord(cap, frames=80):
     accum_frame = None
     for _ in range(frames):
         # Capture frame-by-frame
         _, frame = cap.read()
 
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        lower_red = np.array([0, 110, 130])
-        upper_red = np.array([10, 255, 255])
-        mask = cv2.inRange(hsv, lower_red, upper_red)
+        lower_blue = np.array([110, 40, 50])
+        upper_blue = np.array([130, 255, 255])
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
         # Bitwise-AND mask and original image
         res = cv2.bitwise_and(frame, frame, mask=mask)
         res = cv2.cvtColor(res, cv2.COLOR_HSV2RGB)
         res = cv2.cvtColor(res, cv2.COLOR_RGB2GRAY)
 
-        ret, res = cv2.threshold(res, 1, 255, cv2.THRESH_BINARY)
-
+        ret, res = cv2.threshold(res, 60, 255, cv2.THRESH_BINARY)
+        # cv2.imshow("res", res)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     pass
         if accum_frame is None:
             accum_frame = res
         else:
             accum_frame += res
-    blurred = cv2.GaussianBlur(accum_frame, (5, 5), 0)
-    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 
+    blurred = cv2.GaussianBlur(accum_frame, (29, 29), 0)
+    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+    # cv2.imshow("res", thresh)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     pass
     # find contours in the thresholded image
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-
-    # assert len(cnts) == 1
 
     # loop over the contours
     for c in cnts:
